76f170640c94880d75998fe8e7493f8b10605ff8
Xinyun <vicxinyun@outlook.com>
2020-09-28 18:33:00 +0200

    calibration compile warning

--- control.c
+++ control.c
@@ -20,11 +20,36 @@ void update_motors(void)
 	motor[3] = ae[3];
 }
 
+uint8_t calibration_counter = 0;
+int16_t sensor_calib = 0;
+int16_t sensor_calibration(int16_t sensor_ori, uint8_t num)//average
+{
+	int16_t sensor_temp = 0, sensor_sum = 0;
+	sensor_temp = sensor_ori;
+	sensor_sum += sensor_temp;
+	calibration_counter++;
+	if(calibration_counter == 3){
+		sensor_calib = sensor_sum / num;
+		calibration_counter = 0;
+	}
+	return sensor_calib;
+}
+
+int16_t phi_calib = 0, theta_calib = 0, psi_calib = 0;
 void run_filters_and_control()
 {
+	sensor_calibration(phi, 3); phi_calib = sensor_calib;
+	printf("| %6d ", phi_calib);
+
+	sensor_calibration(theta, 3); theta_calib = sensor_calib;
+	printf("| %6d ", theta_calib);
+
+	sensor_calibration(psi, 3); psi_calib = sensor_calib;
+	printf("| %6d ", psi_calib);
 	// fancy stuff here
 	// control loops and/or filters
 
 	// ae[0] = xxx, ae[1] = yyy etc etc
 	update_motors();
 }
+
